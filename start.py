5b7c04a8232184ab998c9cda8c976edd471429fd
Ligerbot <lawrence.shadowcroft@proton.me>
2024-06-20 09:50:59 -0700

    Fixed trees AGAIN

--- start.py
+++ start.py
@@ -336,14 +336,14 @@ def tree(x,y):
 	turtle.right(180)
 	turtle.forward(r*block_height*turtle_size_move-turtle_size_move)
 	turtle.goto(x-(3*block_width*turtle_size_move),y+(2*block_height*turtle_size_move))
-	if random.randint(1,2) == 2:
+	if random.randint(1,3) == 2 or random.randint(1,3) == 3:
                 turtle.goto(turtle.xcor(), int(turtle.ycor()) - random.randint(-1,1) * block_height*turtle_size_move)
                 turtle.setheading(0)
                 leaf()
                 leaf()
                 leaf()
-                leaf()
-                leaf()
+#                leaf()
+#                leaf()
 
 	turtle.setheading(0)
 	leaf()
